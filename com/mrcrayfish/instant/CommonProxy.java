0e3347b118a75c83764c0cf411d51c29b9819632
MrCrayfish <mrcrayfish@hotmail.com>
2013-07-08 09:11:18 +0930

    Removed other projects

--- com/mrcrayfish/instant/CommonProxy.java
+++
@@ -1,6 +0,0 @@
-package com.mrcrayfish.instant;
-
-public class CommonProxy 
-{
-    public void registerRenderers() {}
-}
--- com/mrcrayfish/tutorial/CommonProxy.java
+++
@@ -1,6 +0,0 @@
-package com.mrcrayfish.tutorial;
-
-public class CommonProxy 
-{
-    public void registerRenderers() {}
-}
